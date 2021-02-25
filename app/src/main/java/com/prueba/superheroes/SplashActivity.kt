092347143108fae1e99e2cd59479e2ff89572828
Javier <fjgonzalezmx@gmail.com>
2021-02-25 10:17:30 -0700

    splash

--- app/src/main/java/com/prueba/superheroes/SplashActivity.kt
+++ app/src/main/java/com/prueba/superheroes/SplashActivity.kt
@@ -8,5 +8,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent(this@SplashActivity, MainActivity::class.java)
-        startActivity(intent)    }
+        startActivity(intent)
+        finish()
+    }
 }
